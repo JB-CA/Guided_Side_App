a18ffd70084a169c7818dfefc09639163de64d13
Jeremy Bell <jeremy.bell@coderacademy.edu.au>
2021-11-05 08:44:55 +1000

    Made password not display when showing serialization

--- gratitude_app/schemas/user_schema.py
+++ gratitude_app/schemas/user_schema.py
@@ -4,10 +4,11 @@ from marshmallow_sqlalchemy import auto_field
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     user_id = auto_field(dump_only=True)
+    password = auto_field(load_only=True)
     class Meta:
         model = User
         load_instance = True
         include_relationships = True
 
-user_schema = UserSchema(exclude=['password'])
-users_schema = UserSchema(exclude=['password'], many=True)
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
