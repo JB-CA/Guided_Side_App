125ce8f0375d5f515a591c4601d61633e89afae1
Jeremy Bell <jeremy.bell@coderacademy.edu.au>
2021-11-13 13:31:21 +1000

    Import and use LoginManager

--- gratitude_app/main.py
+++ gratitude_app/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_login import LoginManager
 from marshmallow.exceptions import ValidationError
 
 db = SQLAlchemy()
 ma = Marshmallow()
+lm = LoginManager()
 
 def create_app():
 
@@ -13,7 +15,8 @@ def create_app():
 
     db.init_app(app)
     ma.init_app(app)
-
+    lm.init_app(app)
+    
     from commands import db_commands
     app.register_blueprint(db_commands)    
     
