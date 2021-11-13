9da21a74c4dc129b579e4a470d2e0fd1131ff2f2
Jeremy Bell <jeremy.bell@coderacademy.edu.au>
2021-11-13 13:30:38 +1000

    password hashing added

--- gratitude_app/schemas/user_schema.py
+++ gratitude_app/schemas/user_schema.py
@@ -2,13 +2,22 @@ from main import ma
 from models.users import User
 from marshmallow_sqlalchemy import auto_field
 from marshmallow.validate import Length, Email, ContainsNoneOf
+from marshmallow.fields import Method
+from marshmallow.exceptions import ValidationError
+from werkzeug.security import generate_password_hash
 import string
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     user_id = auto_field(dump_only=True)
     name = auto_field(required=True, validate=[Length(min=2, max=50), ContainsNoneOf(string.punctuation)])
     email = auto_field(required=True, validate=Email())
-    password = auto_field(load_only=True, required=True, validate=[Length(min=6, max=50)])
+    password = Method(required=True, load_only=True, deserialize="load_password")
+
+    def load_password(self, value):
+        if len(value) > 6:
+            return generate_password_hash(value, method='sha256')
+        raise ValidationError("Password must be at least 6 characters long")
+
     class Meta:
         model = User
         load_instance = True
@@ -16,3 +25,4 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
+user_update_schema = UserSchema(partial=True)
