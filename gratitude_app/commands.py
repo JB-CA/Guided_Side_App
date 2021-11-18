39e672c9968d349fc2bde7a5c87395ee5cd57d8b
Jeremy Bell <jeremy.bell@coderacademy.edu.au>
2021-11-18 13:10:00 +1000

    Added migrations

--- gratitude_app/commands.py
+++ gratitude_app/commands.py
@@ -1,7 +1,7 @@
 from main import db
 from flask import Blueprint
 
-db_commands = Blueprint("db", __name__)
+db_commands = Blueprint("db-custom", __name__)
 
 @db_commands.cli.command("create")
 def create_db():
@@ -13,6 +13,7 @@ def create_db():
 def drop_db():
     """Drops the database."""
     db.drop_all()
+    db.engine.execute("DROP TABLE IF EXISTS alembic_version;")
     print("Tables deleted")
 
 @db_commands.cli.command("seed")
@@ -54,6 +55,7 @@ def seed_db():
 def reset_db():
     """Resets the database."""
     db.drop_all()
+    db.engine.execute("DROP TABLE IF EXISTS alembic_version;")
     db.create_all()    
     from models.users import User
     from models.gratitudes import Gratitude
